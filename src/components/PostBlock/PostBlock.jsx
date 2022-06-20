25511a7e14760d42fff87c0acb826b713d393eda
PlevakoVitalii <plevakovl2102@gmail.com>
2022-06-20 23:30:41 +0300

    fix: change layout for PostBlock component

--- src/components/PostBlock/PostBlock.jsx
+++ src/components/PostBlock/PostBlock.jsx
@@ -6,42 +6,44 @@ const PostBlock = () => {
   return (
     <div className={style.postBlock}>
       <h2>Working with POST request</h2>
-      <form >
+      <form action="#" method="post" encType="multipart/form-data">
         <div className={style.inputContainer}>
-          <input type="text" id="Your_name" name="Your name" value="Your name" />
-          <input type="email" id="email" name="email" value="email" />
-          <input type="phone" id="phone" name="phone" value="phone" />
+          <input type="text" id="Your_name" name="Your name" />
+          <input type="email" id="email" name="email" />
+          <input type="phone" id="phone" name="phone"
+            onChange={(e) => e.target.value} />
         </div>
         <div className={style.phoneInputLabel}>
-          <label for="phone">phone</label>
+          <label htmlFor="phone">phone</label>
         </div>
 
         <span className={style.inputRadioContainerHeader}>Select your position</span>
 
         <div className={style.inputRadioContainer}>
           <div >
-            <input type="radio" id="Frontend_developer" name="position" value="Frontend developer" />
-            <label for="Frontend_developer">Frontend developer</label>
+            <input type="radio" id="Frontend_developer" name="position" value="Frontend developer"
+              onChange={(e) => e.target.value} />
+            <label htmlFor="Frontend_developer">Frontend developer</label>
           </div>
           <div>
             <input type="radio" id="Backend_developer" name="position" value="Backend developer" />
-            <label for="Backend_developer">Backend developer</label>
+            <label htmlFor="Backend_developer">Backend developer</label>
           </div>
           <div>
             <input type="radio" id="Designer" name="position" value="Designer" />
-            <label for="Designer">Designer</label>
+            <label htmlFor="Designer">Designer</label>
           </div>
           <div>
             <input type="radio" id="QA" name="position" value="QA" />
-            <label for="QA">QA</label>
+            <label htmlFor="QA">QA</label>
           </div>
         </div>
+
         <div className={style.inputUploadContainer}>
-          <label for="file">Uploud </label>
-          <input type="file" id="file" name="file" multiple />
+          <input type="file" name="Upload" id="Upload" className={style.inputUploadFile} />
         </div>
-        <div >
-          <button>Sing up</button>
+        <div>
+          <button type="submit">Sing up</button>
         </div>
 
 
